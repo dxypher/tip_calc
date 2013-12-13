5c5be2c7b1db2143053791a60c466484d1ae9646
Nikhil <Home@c-24-5-28-219.hsd1.ca.comcast.net>
2013-12-13 11:12:16 -0800

    initial commit

---
+++ tip_calc/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tip_calc
+//
+//  Created by Nikhil on 12/13/13.
+//  Copyright (c) 2013 Nikhil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
